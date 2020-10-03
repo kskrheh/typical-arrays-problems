c061bd74d152384c1af3f0bb42b2f4a111f1df83
kskrheh <kskrheh@gmail.com>
2020-10-03 19:02:10 +0300

    feat: Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+exports.min = function min(array) {
+    return isValidArray(array) ? Math.min(...array) : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return isValidArray(array) ? Math.max(...array) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!isValidArray(array)) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
+};
+
+const isValidArray = (array) => Array.isArray(array) && array.length > 0;
